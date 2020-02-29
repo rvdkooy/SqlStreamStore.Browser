8c00e9aec184ca9336140732bcacdf4c4dcf4d75
Ronald van der Kooij <ronald@sharpsolutions.nl>
2020-02-29 15:51:58 +0100

    removed unused using statements

--- src/SqlStreamStore.Browser2/BrowserMiddleware.cs
+++ src/SqlStreamStore.Browser2/BrowserMiddleware.cs
@@ -1,16 +1,6 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-// using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Microsoft.AspNetCore.Routing;
 
 namespace SqlStreamStore.Browser2
 {
@@ -18,39 +8,13 @@ namespace SqlStreamStore.Browser2
     {
         public static IApplicationBuilder UseSqlStreamStoreBrowser(
             this IApplicationBuilder builder
-            // IStreamStore streamStore,
-            // SqlStreamStoreMiddlewareOptions options = default
         )
         {
             if(builder == null)
                 throw new ArgumentNullException(nameof(builder));
-            // if(streamStore == null)
-            //     throw new ArgumentNullException(nameof(streamStore));
 
             return builder
-                // .UseExceptionHandling()
                 .UseMvc();
-                    // .MapMiddlewareRoute(
-                    //     Constants.Paths.AllStream,
-                    //     inner => inner.UseAllStream(allStream))
-                    // .MapMiddlewareRoute(
-                    //     $"{Constants.Paths.AllStream}/{{position:long}}",
-                    //     inner => inner.UseAllStreamMessage(allStreamMessages))
-                    // .MapMiddlewareRoute(
-                    //     Constants.Paths.Streams,
-                    //     inner => inner.UseStreamBrowser(streamBrowser))
-                    // .MapMiddlewareRoute(
-                    //     $"{Constants.Paths.Streams}/{{streamId}}",
-                    //     inner => inner.UseStreams(streams))
-                    // .MapMiddlewareRoute(
-                    //     $"{Constants.Paths.Streams}/{{streamId}}/{Constants.Paths.Metadata}",
-                    //     inner => inner.UseStreamMetadata(streamMetadata))
-                    // .MapMiddlewareRoute(
-                    //     $"{Constants.Paths.Streams}/{{streamId}}/{{p}}",
-                    //     inner => inner.UseStreamMessages(streamMessages))
-                    // .MapMiddlewareRoute(
-                    //     string.Empty,
-                    //     inner => inner.UseIndex(index)));
         }
         public static IServiceCollection AddSqlStreamStoreBrowser(
             this IServiceCollection serviceCollection,
