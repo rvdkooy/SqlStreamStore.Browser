d4ce8bb9d76d9666da889ef2676489e987b47608
Ronald van der Kooij <ronald@sharpsolutions.nl>
2020-03-26 22:07:29 +0100

    -it to -i

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 docker build --tag sssb-build .
-docker run --rm -it --name sssb-build \
+docker run --rm -i --name sssb-build \
  -v /var/run/docker.sock:/var/run/docker.sock \
  -v $PWD/artifacts:/repo/artifacts \
  -v $PWD/.git:/.git \
