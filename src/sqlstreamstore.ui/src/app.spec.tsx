ee6878347c002b1da459483ecf535d62f14f5064
rvdkooy <ronald@sharpsolutions.nl>
2020-04-30 00:42:37 +0200

    fixed specs

--- src/sqlstreamstore.ui/src/app.spec.tsx
+++ src/sqlstreamstore.ui/src/app.spec.tsx
@@ -1,19 +1,27 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import { act } from 'react-dom/test-utils';
-import { triggerMessage } from './components/messages/snackBar';
+import { triggerSuccessMessage, triggerErrorMessage } from './components/messages/snackBar';
 import App from './App';
 
 describe('App specs', () => {
-  it('should show a snackbar message when triggered', () => {
+  it('should show a successfull snackbar message when triggered', () => {
     jest.spyOn(console, 'log').mockReturnValue();
     const container = render(<App />);
     
     act(() => {
-      triggerMessage({
-        message: 'test message',
-        severity: 'info',
-      });
+      triggerSuccessMessage('test message');
+    });
+      
+    expect(container.findByText('test message')).toBeTruthy();
+  });
+
+  it('should show an error snackbar message when triggered', () => {
+    jest.spyOn(console, 'log').mockReturnValue();
+    const container = render(<App />);
+    
+    act(() => {
+      triggerErrorMessage('test message');
     });
       
     expect(container.findByText('test message')).toBeTruthy();
